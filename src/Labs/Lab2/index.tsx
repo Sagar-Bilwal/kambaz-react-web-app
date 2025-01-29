e1790f5f683a779098b80de8ca69f1f2bb12c449
Sagar-Bilwal <bilwal.sagar@gmail.com>
2025-01-28 23:43:07 -0500

    Assignment 2 completed till Z index

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,9 +1,14 @@
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Float from "./Float";
 import ForegroundColors from "./ForegroundColors";
 import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -65,5 +70,10 @@ export default function Lab2() {
         <Borders/>
         <Padding/>
         <Margins/>
+        <Corners/>
+        <Dimensions/>
+        <Positions/>
+        <Zindex/>
+        <Float/>
 
     </div> );}
