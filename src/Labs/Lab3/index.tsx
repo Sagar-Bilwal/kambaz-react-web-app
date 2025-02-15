7466fb1416700710376d0d2adb082e1980a76c8e
Sagar-Bilwal <bilwal.sagar@gmail.com>
2025-02-15 02:19:30 -0500

    Completed till 3.3.10

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -4,16 +4,21 @@ import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import FilterFunction from "./FilterFunction";
 import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
+import House from "./House";
 import IfElse from "./IfElse";
 import { ImpliedReturn } from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -39,6 +44,11 @@ export default function Lab3() {
       <MapFunction/>
       <FindFunction/>
       <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <TodoItem/>
+      <TodoList/>
     </div>
   );
 }
