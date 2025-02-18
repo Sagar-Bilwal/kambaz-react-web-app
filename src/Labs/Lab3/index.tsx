9a59deb55808fdd5b7d69cf2362d064b9dd80f89
Sagar-Bilwal <bilwal.sagar@gmail.com>
2025-02-18 01:10:12 -0500

    Completed till 3.7.1

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -13,14 +13,17 @@ import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
 import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
 import House from "./House";
 import IfElse from "./IfElse";
 import { ImpliedReturn } from "./ImpliedReturn";
 import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
 import SimpleArrays from "./SimpleArrays";
 import Spreading from "./Spreading";
+import Square from "./Square";
 import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
@@ -30,6 +33,7 @@ import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div id="wd-lab3" className="ps-4">
       <h3>Lab 3</h3>
@@ -63,6 +67,16 @@ export default function Lab3() {
       <Classes/>
       <Styles/>
       <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <hr />
+     <PathParameters/>
+     <hr />
     </div>
   );
 }
