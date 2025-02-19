ba4dd9b7b99356348936ce01e872ecdc7814e799
Sagar-Bilwal <bilwal.sagar@gmail.com>
2025-02-19 01:07:30 -0500

    removed unused import

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 export default function CourseNavigation() {
   const { cid } = useParams()
-  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+  // const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       <Link to={`/Kambaz/Courses/${cid}/Home`} id="wd-course-home-link"
--- src/Labs/Lab2/Positions.tsx
+++ src/Labs/Lab2/Positions.tsx
@@ -1,4 +1,3 @@
-
 export default function Positions() {
     return (
         <div>
