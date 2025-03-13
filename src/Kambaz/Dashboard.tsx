50e7fefe3f2805e29aaa0dd2fc3d5464f9d98519
Sagar-Bilwal <bilwal.s@northeastern.edu>
2025-03-13 02:59:45 -0400

    Completed Till 4.4.6

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,24 +1,25 @@
 import { Button, Card, Col, FormControl, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as db from "./Database";
+import { addNewCourse, deleteCourse, updateCourse } from "./Courses/courseReducer";
+import { useState } from "react";
 
-export default function Dashboard({ courses, course, setCourse, addNewCourse,
-  deleteCourse, updateCourse }: {
-  courses: any[]; course: any; setCourse: (course: any) => void;
-  addNewCourse: () => void; deleteCourse: (course: any) => void;
-  updateCourse: () => void; }) {
+export default function Dashboard() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { enrollments } = db;
+  const dispatch = useDispatch();
+  const { courses } = useSelector((state: any) => state.courseReducer);
+  const [course, setCourse] = useState<any>({});
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       {currentUser != null && currentUser.role === "FACULTY" && <div><h5>New Course
           <button className="btn btn-primary float-end"
                   id="wd-add-new-course-click"
-                  onClick={addNewCourse} > Add </button>
+                  onClick={() => dispatch(addNewCourse(course))} > Add </button>
           <button className="btn btn-warning float-end me-2"
-                onClick={updateCourse} id="wd-update-course-click">
+                onClick={() => dispatch(updateCourse(course))} id="wd-update-course-click">
           Update
         </button>
       </h5><br />
@@ -26,17 +27,23 @@ export default function Dashboard({ courses, course, setCourse, addNewCourse,
       onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
       <FormControl as="textarea" value={course.description} rows={3}
       onChange={(e) => setCourse({ ...course, description: e.target.value }) } /><hr /></div>}
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses
+        .filter((course: any) =>
+        enrollments.some(
+          (enrollment) =>
+            enrollment.user === currentUser._id &&
+            enrollment.course === course._id
+          )).length})</h2> <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
           {courses
-        .filter((course) =>
+        .filter((course: any) =>
         enrollments.some(
           (enrollment) =>
             enrollment.user === currentUser._id &&
             enrollment.course === course._id
           ))
-        .map((course) => (
+        .map((course: any) => (
               <Col className="wd-dashboard-course" style={{ width: "300px" }}>
                 <Card>
                   <Link to={`/Kambaz/Courses/${course._id}/Home`}
@@ -60,7 +67,7 @@ export default function Dashboard({ courses, course, setCourse, addNewCourse,
 
                       <button onClick={(event) => {
                                 event.preventDefault();
-                                deleteCourse(course._id);
+                                dispatch(deleteCourse(course._id));
                               }} className="btn btn-danger"
                               id="wd-delete-course-click">
                               Delete
